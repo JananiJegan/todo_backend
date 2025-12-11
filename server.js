c1fedc5fe7b354b7c55da20c99f6b84c5cb004b1
JananiJegan <166229070+JananiJegan@users.noreply.github.com>
2025-12-11 11:11:54 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const router=require('./routes/tasks')
+const router = require('./routes/tasks');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-MONGO_URI=process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.use(cors(
-    methods=['GET', 'POST', 'PUT', 'DELETE'],
-    origin= '*'
-));
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
 app.use(express.json());
 
-mongoose.connect(MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    }); 
-})
-.catch((err)=>{
-    console.error("Error connecting to MongoDB", err);
-})
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
 
-app.use('/api/tasks',router);
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB", err);
+    });
+
+app.use('/api/tasks', router);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    });
+}
